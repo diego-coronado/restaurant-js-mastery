fdc17fbfb1e315ca5e0a785880af837facee89a8
Diego Coronado <dcoronado@able.co>
2022-08-09 18:30:52 +0900

    add aboutus

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -4,7 +4,7 @@ import { images } from "../../constants";
 const SubHeading = ({ title }) => (
   <div style={{ marginBottom: "1rem" }}>
     <p className="p__cormorant">{title}</p>
-    <ima src={images.spoon} alt="spoon" className="spoon__img" />
+    <img src={images.spoon} alt="spoon" className="spoon__img" />
   </div>
 );
 
